a65b584049dbe3d7e219defc758b7303a5414ba6
minihauner <jannickhaunstrup@gmail.com>
2024-10-15 20:56:57 +0200

    tilføj footer, åbn- og lukkefunktion til mobil-header + styling

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const mobileNavToggle = document.getElementById('mobile-nav-toggle');
+const mobileNavClose = document.getElementById('mobile-nav-close');
+const mobileNav = document.getElementById('mobile-nav');
+
+mobileNavToggle.addEventListener('click', () => {
+    if (!mobileNav.classList.contains('main-nav-mobile--open')) {
+        mobileNav.classList.add('main-nav-mobile--open');
+    }
+});
+
+mobileNavClose.addEventListener('click', () => {
+    if (mobileNav.classList.contains('main-nav-mobile--open')) {
+        mobileNav.classList.remove('main-nav-mobile--open');
+    }
+});
